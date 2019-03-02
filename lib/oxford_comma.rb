84ba09e18d024e1ce26aa49eb7f9435b1fc4d186
Annie Phan <anniephan.atp@gmail.com>
2019-03-02 08:24:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,27 @@ def oxford_comma(array)
     return array.join
   elsif array.length == 2
     return array.join(" and ")
-    
+  elsif array.length >= 3
+    array.pop
+    new_last_arr_item = " and #{array[-1]}"
+    array << new_last_arr_item
+    return array.join(", ")
 
+end
+
+def oxford_comma(arr)
+  if arr.length >= 3
+    arr.pop
+    new_item = " and #{arr[-1]}"
+    arr << new_item
+    return arr.join(", ")
+  end
+end
+
+def oxford_comma(arr)
+  if arr.length >= 3
+    arr << "and #{arr[-1]}"
+    arr.delete_at(-2)
+    return arr.join(", ")
+  end
 end
