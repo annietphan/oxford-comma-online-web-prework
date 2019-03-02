936b9e3588f065595b7731eece336563c18a7e69
Annie Phan <anniephan.atp@gmail.com>
2019-03-02 08:36:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,29 +1,11 @@
 def oxford_comma(array)
   if array.length == 1
-    return array.join
+    return array.first
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length >= 3
-    array.pop
-    new_last_arr_item = " and #{array[-1]}"
-    array << new_last_arr_item
+    array << "and #{array[-1]}"
+    array.delete_at(-2)
     return array.join(", ")
-
-end
-
-def oxford_comma(arr)
-  if arr.length >= 3
-    arr.pop
-    new_item = " and #{arr[-1]}"
-    arr << new_item
-    return arr.join(", ")
-  end
-end
-
-def oxford_comma(arr)
-  if arr.length >= 3
-    arr << "and #{arr[-1]}"
-    arr.delete_at(-2)
-    return arr.join(", ")
   end
 end
